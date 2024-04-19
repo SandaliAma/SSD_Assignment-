917edadf174c057dd9fc175706a83e0c5396d9ad
Ashan Gunathilaka <ashangunathilaka29@gmail.com>
2024-04-19 13:21:14 +0530

    update_class

--- frontend/src/screen/Student/Enroll_Mangement/Test.jsx
+++ frontend/src/screen/Student/Enroll_Mangement/Test.jsx
@@ -1,32 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from React Router
 import './Test.css';
 
 function Test() {
   return (
     <div>
-    <div className="buttontest">
-      <h2>Sinhala</h2>
-      <button className="button">View Class</button>
-      <button className="button">View schedule</button>
-      <br/><br/>
-      <h2>Maths</h2>
-      <button className="button">View Class</button>
-      <button className="button">View schedule</button>
-      <br/><br/>
-      <h2>ICT</h2>
-      <button className="button">View Class</button>
-      <button className="button">View schedule</button>
-      <br/><br/>
-      <h2>English</h2>
-      <button className="button">View Class</button>
-      <button className="button">View schedule</button>
-
-      
-      
-    </div>
-    <div>
-    <button className="button">Enrolled Class</button>
-    </div>
+      <div className="buttontest">
+        <h2>Sinhala</h2>
+        <Link to="/viewclass"><button className="button">View Class</button></Link> {/* Link to Sinhala class */}
+        <Link to="/"><button className="button">View Schedule</button></Link> {/* Link to Sinhala schedule */}
+        <br/><br/>
+        <h2>Maths</h2>
+        <Link to="/"><button className="button">View Class</button></Link> {/* Link to Maths class */}
+        <Link to="/"><button className="button">View Schedule</button></Link> {/* Link to Maths schedule */}
+        <br/><br/>
+        <h2>ICT</h2>
+        <Link to="/"><button className="button">View Class</button></Link> {/* Link to ICT class */}
+        <Link to="/"><button className="button">View Schedule</button></Link> {/* Link to ICT schedule */}
+        <br/><br/>
+        <h2>English</h2>
+        <Link to="/"><button className="button">View Class</button></Link> {/* Link to English class */}
+        <Link to="/"><button className="button">View Schedule</button></Link> {/* Link to English schedule */}
+      </div>
+      <div>
+        <Link to="/enrolled"><button className="button">Enrolled Class</button></Link> {/* Link to enrolled classes */}
+      </div>
     </div>
   );
 }
---
+++ frontend/src/screen/Student/Enroll_Mangement/ViewClass.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'; // Import Link from React Router
+
+function ViewClass() {
+  return (
+    <div>
+     <h2>PayNow</h2>
+        <Link to="/payonline"><button className="button">PayNow</button></Link> {/* Link to Maths class */}
+
+    </div>
+  )
+}
+
+export default ViewClass
