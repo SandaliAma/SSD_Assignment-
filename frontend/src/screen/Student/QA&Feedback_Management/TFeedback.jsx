2ce47d3f23b972a6849f109ff0c98a29e48b37d2
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-04-22 23:41:00 +0530

    Update TFeedback

--- frontend/src/screen/Student/QA&Feedback_Management/TFeedback.jsx
+++ frontend/src/screen/Student/QA&Feedback_Management/TFeedback.jsx
@@ -1,24 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import './TFeedback.css';
-import {Link } from 'react-router-dom';
 import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
-import toast, { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
 import Swal from 'sweetalert2';
 import Head from '../Header/Header';
 
 function TFeedback() {
 
   const [grade, setGrade] = useState();
-  const [subject, setSubject] = useState();
-  const [teacher, setTeacher] = useState();
+  const [subject, setSubject] = useState('');
+  const [teacherid, setTeacherid] = useState([]);
+  const [teacher, setTeacher] = useState('');
   const [sid, setSid] = useState();
   const [tfeedback, setTFeedback] = useState();
   const navigator = useNavigate();
 
   const submit = (a) =>{
     a.preventDefault();
-    axios.post('http://localhost:5000/createTF', {grade:grade,subject:subject,teacher:teacher,sid:sid,feedback:tfeedback})
+    axios.post('http://localhost:5000/createTF', {
+      grade:grade,
+      subject:subject,
+      teacher:teacher,
+      sid:sid,
+      feedback:tfeedback})
     .then(res =>{
       console.log(res);
       console.log( `Feedback Submitted successfully.`);
@@ -92,6 +97,39 @@ function TFeedback() {
     }, 5000); // Wait for 5 seconds before dismissing loading toast
   };
 
+  useEffect(()=>{
+    axios.get('/studentprofile')
+    .then((res)=>{
+      setSid(res.data.stdid);   
+      setGrade(res.data.grade);       
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+  },[])
+
+  useEffect(()=>{
+    axios.get('/teacherprofileall')
+    .then((res)=>{
+      setTeacherid(res.data);             
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+  },[])
+
+  useEffect(() => {
+    if (teacher) {
+      axios.get('/teacherprofileall')
+        .then(res => {
+          const selectedTeacher = res.data.find(t => t.name === teacher);
+          if (selectedTeacher) {
+            setSubject(selectedTeacher.subject);
+          }
+        })
+        .catch(err => console.error(err));
+    }
+  }, [teacher]);
 
   /*const[questions,setQuestions] = useState([]);
   useEffect(() => {
@@ -103,55 +141,33 @@ function TFeedback() {
   },[]);*/
 
   return (
+    <>
+    <Head/>
     <div className='uth1'>
-       <Head/>
+      
         <body >
       <h1 className="heading8">We Want to Hear from You - Teacher Feedback</h1>
       <form onSubmit={handleSubmit}>
         
-        <label htmlFor="grade" className="tt1">Select Grade</label>
-        <select id="grade" required name="dropdown" style={{ position: 'absolute', width: '337px', height: '37px', left: '632px', top: '200px', background: '#FFFFFF', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '8px' }} onChange={(a)=> setGrade(a.target.value)}>
-        <option value="" ></option>
-        <option value="Grade 4" >Grade 4</option>
-          <option value="Grade 5" >Grade 5</option>
-          <option value="Grade 6" >Grade 6</option>
-          <option value="Grade 7" >Grade 7</option>
-          <option value="Grade 8" >Grade 8</option>
-          <option value="Grade 9" >Grade 9</option>
-          <option value="Grade 10" >Grade 10</option>
-          <option value="Grade 11" >Grade11</option>
-        </select>
+        <label htmlFor="grade" className="tt1">Select Grade</label>        
+        <input id="dropdown1" name="dropdown" value={grade}
+        style={{ position: 'absolute', width: '351px', height: '40px', left: '632px', top: '200px', border: '1px solid #000000', borderRadius: '10px' }}  readOnly/>
         
-        <label htmlFor="subject" className="tt2">Select Subject</label>
-        <select id="subject" required name="dropdown" style={{ position: 'absolute', width: '337px', height: '37px', left: '632px', top: '264px', background: '#FFFFFF', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '8px' }}  onChange={(a)=> setSubject(a.target.value)}>
-        <option value="" ></option>
-        <option value="History" >History</option>
-          <option value="Sinhala" >Sinhala</option>
-          <option value="ICT" >ICT</option>
-          <option value="Music" >Music</option>
-          <option value="Mathematics" >Mathematics</option>
-          <option value="Science" >Science</option>
-          <option value="English" >English</option>
+        <label htmlFor="teacher" className="tt2">Select Teacher</label>
+        <select id="dropdown3" name="dropdown" style={{ position: 'absolute', width: '351px', height: '40px', left: '632px', top: '264px', background: '#FFFFFF', border: '1px solid #000000', borderRadius: '10px' }} required onChange={(a)=> setTeacher(a.target.value)}>
+         
+        <option value=""></option>
+        {teacherid.map((teache, index) => (
+          <option key={index} value={teache.name}>{teache.name}</option>
+        ))}
         </select>
-        
-        <label htmlFor="teacher" className="tt3">Select Teacher</label>
-        <select id="teacher" required name="dropdown" style={{ position: 'absolute', width: '337px', height: '37px', left: '632px', top: '346px', background: '#FFFFFF', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '8px' }} onChange={(a)=> setTeacher(a.target.value)}>
-        <option value="" ></option>
-          <option value="Mr.Amila" >Mr.Amila</option>
-          <option value="Mrs.Nimal" >Mrs.Nimal</option>
-          <option value="Mrs.Upul" >Mrs.Upul</option>
-          <option value="Mr.Senaka" >Mr.Senaka</option>
-          <option value="Mrs.Anne" >Mrs.Anne</option>
-        </select>
+
+        <label htmlFor="subject" className="tt3">Select Subject</label>
+        <input id="dropdown1" name="dropdown" value={subject} style={{ position: 'absolute', width: '351px', height: '40px', left: '632px', top: '346px', background: '#FFFFFF', border: '1px solid #000000', borderRadius: '10px' }}  onChange={(a)=> setSubject(a.target.value)} readOnly/>
         
         <label htmlFor="studentID" className="tt4">Student ID</label>
-        <input
-          id="studentID"
-          pattern="^SD\d{3}$"
-          style={{ boxSizing: 'border-box', position: 'absolute', width: '337px', height: '53px', left: '632px', top: '448px', background: '#FFFFFF', border: '1px solid #000000' }}
-          type="text"
-          onChange={(a)=> setSid(a.target.value)}
-        />
+        <input type="text" name="sSID" pattern="^SD\d{3}$" title="Please enter 'SD001'" value={sid} style={{ boxSizing: 'border-box', position: 'absolute', width: '351px', height: '53px', left: '636px', top: '448px', background: '#FFFFFF', border: '1px solid #000000', borderRadius: '10px' }} readOnly/>
+        
         <label htmlFor="feedback" className="tt5">Feedback</label>
         <textarea
           id="feedback"
@@ -169,6 +185,7 @@ function TFeedback() {
       </form>
     </body>
     </div>
+    </>
   )
 }
 
