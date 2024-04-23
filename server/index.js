e06762122806f338c6b7fd4c9c305c530184d006
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-04-23 21:41:39 +0530

    update port

--- server/index.js
+++ server/index.js
@@ -278,5 +278,5 @@ if(process.env.NODE_ENV === 'production') {
 }
  
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
