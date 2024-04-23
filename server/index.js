b45260100403229ea05dffaadc98ef8b28b8565e
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-04-23 21:22:16 +0530

    Heroku

--- server/index.js
+++ server/index.js
@@ -270,11 +270,18 @@ app.delete('/deletephoto/:id', (req, res) => {
       });
 });
 
-__dirname = path.resolve();
+// __dirname = path.resolve();
+// if(process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+//   app.get('*', (req, res) => {
+//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+//   });
+// }
+
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+  app.get('*', (req, res) =>{
+      res.sendFile(path.join(__dirname,"frontend","build","index.html"));
   });
 }
  
