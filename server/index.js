c6d888b96614f16ee925b45df690627f05c6b653
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-04-23 21:28:21 +0530

    delete build

--- server/index.js
+++ server/index.js
@@ -270,14 +270,6 @@ app.delete('/deletephoto/:id', (req, res) => {
       });
 });
 
-// __dirname = path.resolve();
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
-//   app.get('*', (req, res) => {
-//       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-//   });
-// }
-
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'frontend', 'build')));
   app.get('*', (req, res) =>{
