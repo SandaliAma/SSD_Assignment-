ddc4936ba406b6419f585a08cbbb248344fc5d9f
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-04-23 17:53:48 +0530

    Heroku

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const app = express();
+const path = require('path');
 const multer = require('multer');
 const UserModelLesson = require('./models/Lesson');
 const BankModel = require('./models/BankPayments');
@@ -270,6 +271,13 @@ app.delete('/deletephoto/:id', (req, res) => {
 });
 
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+ 
 
 const port = 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
