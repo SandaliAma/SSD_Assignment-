6a0b29cb2d1e9b0ff38eda00c876c18e2ac291fb
Ashan Gunathilaka <ashangunathilaka29@gmail.com>
2024-05-04 19:04:37 +0530

    update1

--- frontend/src/screen/Student/NavBar/Nav.jsx
+++ frontend/src/screen/Student/NavBar/Nav.jsx
@@ -33,7 +33,7 @@ function Nav() {
                     </li>
                     <li>
                         <img src={classes} alt='home' className='navimage'/>
-                        <a href='/test'>My Classes</a>
+                        <a href='/MyClass'>My Classes</a>
                     </li>
                     {/* <li>
                         <img src={classes} alt='home' className='navimage'/>
@@ -41,7 +41,7 @@ function Nav() {
                     </li> */}
                     <li>
                         <img src={enroll} alt='home' className='navimage'/>
-                        <a href='/login'>Enrollments</a>
+                        <a href='/Enrollments'>Enrollments</a>
                     </li>
                     <li>
                         <img src={pay} alt='home' className='navimage'/>
