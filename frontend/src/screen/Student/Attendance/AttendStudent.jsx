cc53e5dd14efdfd885444fb22393e001fe3bf119
wolferlk <sasiofficial25@gmail.com>
2024-05-06 10:13:10 +0530

    sasindu css

--- frontend/src/screen/Student/Attendance/AttendStudent.jsx
+++ frontend/src/screen/Student/Attendance/AttendStudent.jsx
@@ -4,6 +4,7 @@ import { toast } from 'react-hot-toast';
 import Head from '../Header/Header';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import '../../../styles/Sasi.css';
+import './attend.css';
 
 
 function AttendStudent() {
@@ -117,13 +118,12 @@ function AttendStudent() {
                     <br />
                     <div className='ViewAttends By Class'>
                         <div className="card text-center">
-                            <div className="card-header">
-                                View Attendance by Class
-                            </div>
+                            
                             <div className="card-body">
                                 <input
                                     type="text"
-                                    className="form-control mb-4"
+                                    //className="form-control mb-4"
+                                    className='srchbr'
                                     placeholder="Search By Class ID Or Subject "
                                     value={classFilter}
                                     onChange={handleClassFilter}
