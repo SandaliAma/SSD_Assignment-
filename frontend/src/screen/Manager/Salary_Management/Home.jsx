b1f37536ef3f96a1ecf0d3b3b235d9c15c17f610
Eshmika Hettiarachchi <119941730+Eshmika@users.noreply.github.com>
2024-05-08 15:08:29 +0530

    upate nimji

--- frontend/src/screen/Manager/Salary_Management/Home.jsx
+++ frontend/src/screen/Manager/Salary_Management/Home.jsx
@@ -7,17 +7,19 @@ function Home() {
   return (
     <div>
     <Head/>
-    <div className="bodyst">
+    <div className="bodyC">
           <div className="nimanji1" className1="container5"><br />
  <Link to={'/create'}>
     <button type="submit" name="AddSalary" className="addsalary">Add Salary</button>
+    <br></br>
+    <br></br>
 </Link>
 <Link to={'/managersalary'}>
     <button type="submit" name="ViewSalary" className="ViewSalary">View Salary</button>
+    <br></br>
+    <br></br>
 </Link>
-<Link to={'/TeacherView'}>
-    <button type="submit" name="GenerateReport" className="generate">Generate Report</button>
-</Link>
+
   </div>
 
     </div>
